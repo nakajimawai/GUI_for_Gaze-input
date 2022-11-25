c9d62e7b3ae31c2ba84a6a58f66fc4c2e96c2205
nakajima <yutonkjima1108@gmail.com>
2022-11-25 18:32:44 +0900

    右旋回と左旋回のボタンを追加

--- GUI.py
+++ GUI.py
@@ -3,17 +3,18 @@ import socket
 from tkinter import *
 from tkinter import ttk
 #Set server ip address, port, buffer capacity
-HOST='192.168.143.152'
+HOST='192.168.11.17'
 PORT=8008
 BUFFER=4096
 
+#文字列送信用
 def control(data):
          # Define socket communication type ipv4, tcp
     soc=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
          #connect to the server 
     soc.connect((HOST,PORT))
          #delay
-    time.sleep(1)
+    #time.sleep(1)
     if data == "exit":
          pass
     else:
@@ -29,7 +30,14 @@ def control(data):
 def forward():
     print("前進")
     control("w")
-
+#ボタンright
+def right():
+    print("右旋回")
+    control("d")
+#ボタンforward
+def left():
+    print("左旋回")
+    control("a")
 #ボタンstop
 def stop():
     print("停止")
@@ -39,24 +47,53 @@ def stop():
 if __name__ == "__main__":
 
     root = Tk()
-    root.geometry('1280x750')
+    root.geometry('1275x765')
     root.title("GUI_for_gaze input")
     #ウィジェット
     frame1 = ttk.Frame(root,padding=10)
     #前進用ボタン
-    button_forward = ttk.Button(
+    button_forward = Button(
         frame1,
-        text="forward",
+        text="FORWARD",
+        font=("",18),
+        fg='red',
+        width=30,
+        height=5,
         command=forward
     )
-    #停止用ボタン
-    button_stop =ttk.Button(
+    #右旋回用ボタン
+    button_right = Button(
         frame1,
-        text="stop",
+        text="RIGHT",
+        font=("",18),
+        fg='purple',
+        width=10,
+        height=15,
+        command=right
+    )#左旋回用ボタン
+    button_left = Button(
+        frame1,
+        text="LEFT",
+        font=("",18),
+        fg='green',
+        width=10,
+        height=15,
+        command=left
+    )
+    #停止用ボタン
+    button_stop =Button(
+        frame1,
+        text="STOP",
+        font=("",18),
+        fg='gold',
+        width=30,
+        height=5,
         command=stop
     )
     #レイアウト
     frame1.pack(expand=True, fill=BOTH)
-    button_forward.pack(side=TOP, ipadx=50, ipady=50)
-    button_stop.pack(side=BOTTOM, ipadx=50, ipady=50)
+    button_forward.pack(side=TOP)
+    button_stop.pack(side=BOTTOM)
+    button_right.pack(side=RIGHT)
+    button_left.pack(side=LEFT)#文字とボタン端までの間隔ipadx=50, ipady=50
     root.mainloop()

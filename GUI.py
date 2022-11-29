be61b43b04cc18245a44a57278602e2496886c40
nakajima <yutonkjima1108@gmail.com>
2022-11-29 21:59:39 +0900

    ボタンの文字変更、ボタンに画像が表示されない状態

--- GUI.py
+++ GUI.py
@@ -2,10 +2,71 @@ import time
 import socket
 from tkinter import *
 from tkinter import ttk
+import cv2
 #Set server ip address, port, buffer capacity
-HOST='192.168.11.17'
+HOST='192.168.11.26'
 PORT=8008
 BUFFER=4096
+
+def init(root):
+    root.geometry('1275x765')
+    root.title("GUI_for_gaze input")
+    #ウィジェット
+    frame1 = ttk.Frame(root,padding=10)
+    #画像読み込み
+    origin_image = PhotoImage(file='sample_arrow.png')
+    print(origin_image)
+    big_img = origin_image.zoom(5,5)
+    small_img = origin_image.subsample(5,5)
+    #前進用ボタン
+    button_forward = Button(
+        frame1,
+        text="FORWARD",
+        font=("",18),
+        fg='red',
+        #image=origin_image,
+        compound="top",
+        width=30,
+        height=5,
+        command=forward
+    )
+    #button_forward.bind("<Button-1>",forward)
+    #右旋回用ボタン
+    button_right = Button(
+        frame1,
+        text="CW",
+        font=("",18),
+        fg='purple',
+        width=10,
+        height=15,
+        command=right
+    )#左旋回用ボタン
+    button_left = Button(
+        frame1,
+        text="CCW",
+        font=("",18),
+        fg='green',
+        width=10,
+        height=15,
+        command=left
+    )
+    #停止用ボタン
+    button_stop =Button(
+        frame1,
+        text="STOP",
+        font=("",18),
+        fg='gold',
+        width=30,
+        height=5,
+        command=stop
+    )
+    #レイアウト
+    frame1.pack(expand=True, fill=BOTH)
+    button_forward.pack(side=TOP)
+    button_stop.pack(side=BOTTOM)
+    button_right.pack(side=RIGHT)
+    button_left.pack(side=LEFT)#文字とボタン端までの間隔ipadx=50, ipady=50
+    #button_forward.bind("Button-1",forward)
 
 #文字列送信用
 def control(data):
@@ -47,53 +108,5 @@ def stop():
 if __name__ == "__main__":
 
     root = Tk()
-    root.geometry('1275x765')
-    root.title("GUI_for_gaze input")
-    #ウィジェット
-    frame1 = ttk.Frame(root,padding=10)
-    #前進用ボタン
-    button_forward = Button(
-        frame1,
-        text="FORWARD",
-        font=("",18),
-        fg='red',
-        width=30,
-        height=5,
-        command=forward
-    )
-    #右旋回用ボタン
-    button_right = Button(
-        frame1,
-        text="RIGHT",
-        font=("",18),
-        fg='purple',
-        width=10,
-        height=15,
-        command=right
-    )#左旋回用ボタン
-    button_left = Button(
-        frame1,
-        text="LEFT",
-        font=("",18),
-        fg='green',
-        width=10,
-        height=15,
-        command=left
-    )
-    #停止用ボタン
-    button_stop =Button(
-        frame1,
-        text="STOP",
-        font=("",18),
-        fg='gold',
-        width=30,
-        height=5,
-        command=stop
-    )
-    #レイアウト
-    frame1.pack(expand=True, fill=BOTH)
-    button_forward.pack(side=TOP)
-    button_stop.pack(side=BOTTOM)
-    button_right.pack(side=RIGHT)
-    button_left.pack(side=LEFT)#文字とボタン端までの間隔ipadx=50, ipady=50
+    init(root)
     root.mainloop()
